7ff1bf77214b3977d6b373f1fc975e9e382276e7
tiemo0708 <tiemo0708@naver.com>
2022-08-18 13:52:44 +0000

    템플릿 메소드패턴 2

--- src/TemplateMethod/AbstGameConnectHelper.java
+++ src/TemplateMethod/AbstGameConnectHelper.java
@@ -12,7 +12,38 @@ public class AbstGameConnectHelper {
 
 	abstract protected String connection(String info);
     
+    //템플릿 메소드
     public String requestConnection(String info) {
         
+        //보안 작업 -> 암호화 된 문자열을 디코드(복호화) 한다
+       String decodedInfo = doSecurity(info);
+        //반환된 것을 가지고 아이디, 암호를 할당한다.
+        
+        String id = "aaa";
+        String password = "bbb";
+        
+      if(authentication(id, password)){
+          throw new Error("아이디 암호 불일치");
+      }
+            
+        String userName = "userName";
+        int result = authorization(userName);
+        
+        switch (result) {
+
+		case 0:// 무료회원
+			break;
+		case 1:// 유료회원
+			break;
+		case 2:// 게임 마스터
+			break;
+		case 3:// 접속 권한 없음
+			break;
+		default:
+			break;
+		}
+        
+            
+        return connection();
     }
 }
