17cbe5e8e8ee8e39fd87019513e8e00f85f19611
tiemo0708 <tiemo0708@naver.com>
2022-09-05 13:51:29 +0000

    팩토리메소드 패턴 2(09/05 22:51)

---
+++ src/FactoryMethod/concreate/HpPotion.java
@@ -0,0 +1,11 @@
+package concreate;
+
+import framework.Item;
+
+public class HpPotion implements Item{
+    
+    @Override
+    public void use(){
+        System.out.println("체력회복");
+    }
+}
--- src/FactoryMethod/framework/ItemCreator.java
+++ src/FactoryMethod/framework/ItemCreator.java
@@ -7,14 +7,21 @@ public abstract class ItemCreator {
     public Item Create(){
         
         Item item;
+        //step1
+        requesttmInfo();
+        item = createItem();
+        //step3
+        createItemLog();
         
         return item;
-        
+        system.
     }
     
     //아이템 생성하기 전에 데이터베이스에서 아이템 정보 요청
     abstract protected void requesttmInfo();
     //아이템 생성 후 아이템 복제 방지하기위 데이터베이스에 아이템 생성정보 저장
-   abstract protected void createItemLog();
+   	abstract protected void createItemLog();
+    //아이템을 생성하는 알고리즘
+    abstract protected Item createItem();
     
 }
