477e87ca416c4887068f29d3daeb92d3c679b5ff
tiemo0708 <tiemo0708@naver.com>
2023-08-15 14:07:18 +0000

    빌더패턴_1(Main+Computer클래스)

---
+++ src/BuilderPattern/Computer.java
@@ -0,0 +1,35 @@
+package BuilderPattern;
+
+public class Computer {
+    
+    private String cpu;
+    private String ram;
+    private String storage;
+
+    
+    public Computer(String cpu, String ram, String storage) {
+        super();
+        this.cpu = cpu;
+        this.ram = ram;
+        this.storage = storage;
+    }
+    public String getCpu() {
+        return cpu;
+    }
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+    public String getRam() {
+        return ram;
+    }
+    public void setRam(String ram) {
+        this.ram = ram;
+    }
+    public String getstorage() {
+        return storage;
+    }
+    public void setstorage(String storage) {
+        this.storage = storage;
+    }
+
+}
---
+++ src/BuilderPattern/Main.java
@@ -0,0 +1,7 @@
+package BuilderPattern;
+
+public class Main {
+    public static void main(String[] args) {
+       
+    }
+}
