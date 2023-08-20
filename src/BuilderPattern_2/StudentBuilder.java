083d0315f6c928cb2e438f9e309a6b8171911fea
tiemo0708 <tiemo0708@naver.com>
2023-08-20 01:38:13 +0000

    BuilderPattern_2

---
+++ src/BuilderPattern_2/StudentBuilder.java
@@ -0,0 +1,31 @@
+package BuilderPattern_2;
+
+public class StudentBuilder {
+    private int id;
+    private String name;
+    private String grade;
+    private String phoneNumber;
+
+    public StudentBuilder id(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public StudentBuilder name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public StudentBuilder grade(String grade) {
+        this.grade = grade;
+        return this;
+    }
+
+    public StudentBuilder phoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+      public Student build() {
+        return new Student(id, name, grade, phoneNumber); // Student 생성자 호출
+    }
+}
